6aeca2ad251d50af6a7921b5016d0ded97091eda
allicen <ellena.hella@yandex.ru>
2021-09-17 03:57:04 +0500

    Отредактирована модель и скетч.

--- src/armbot_move/control-arduino/move_arduino_2/move_arduino_2.ino
+++ src/armbot_move/control-arduino/move_arduino_2/move_arduino_2.ino
@@ -7,21 +7,22 @@
 #include <sensor_msgs/JointState.h>
 #include <std_msgs/String.h>
 
-// 1 joint
+// 1 joint (шаговый двигатель)
 #define Z_STEP_PIN   46
 #define Z_DIR_PIN    48
 #define Z_ENABLE_PIN 62
 
-// 2 joint
+// 2 joint (шаговый двигатель)
 #define X_STEP_PIN   54
 #define X_DIR_PIN    55
 #define X_ENABLE_PIN 38
 
-// 3 joint
+// 3 joint (шаговый двигатель)
 #define Y_STEP_PIN   60
 #define Y_DIR_PIN    61
 #define Y_ENABLE_PIN 56
 
+// Сервоприводы
 #define JOINT_END_PIN 11
 #define JOINT_GRIP_PIN 6
 
@@ -30,10 +31,15 @@
 #define SERVO_COUNT 2
 
 #define SPEED 12000
+#define ACCELERATION 1000
 #define M_PI 3.14159265358979323846
 #define RADIAN 57.3
 
-std_msgs::String str; 
+// 1 градус = 46 шагов
+// 200 шагов * 5,18 передаточное отношение * 16 микрошаг / 360 градусов
+#define STEP_IN_ANGLE 46
+
+std_msgs::String str;
 ros::NodeHandle nodeHandle;
 
 // Кнопка и светодиод
@@ -52,9 +58,6 @@ MultiStepper steppers;
 Servo robotServos[SERVO_COUNT];
 
 
-long positions[5]; // 3 ШД и 2 сервопривода
-
-
 //////////// для отладки
 float joint_1_rem;
 float joint_2_rem;
@@ -66,21 +69,54 @@ float joint_grip_rem;
 
 //////////////// start
 
-int servo_pins[SERVO_COUNT] = {JOINT_END_PIN, JOINT_GRIP_PIN}; // PWM Pins
+int servo_pins[SERVO_COUNT] = {JOINT_END_PIN, JOINT_GRIP_PIN}; // Servo Pins
 
 int motorCurrentPositions[MOTOR_COUNT] = {0, 0, 0, 0, 0};
 float targetJointPositions[MOTOR_COUNT] = {0, 0, 0, 0, 0};
+long positions[3] = {0, 0, 0};
 
 
 /////////// end
 
-// Convert the joint state values to degrees, adjust for the center and write to the servo
+// Блокирующая функция, которая работает только с ускорением (!!!!!)
+// Эта функция, скорее всего, не подойдет (тк блокирующая)
+void stepperMoveTo (AccelStepper stepper, long stepCount) {
+    stepper.moveTo(stepCount);
+    while (stepper.currentPosition() != stepCount) {
+        stepper.run();
+        buttonPressed();
+        if (!ledOn) {
+            break;
+        }
+    }
+}
+
+// Зспуск двигателя без ускорения
+void stepperRun (AccelStepper stepper, long stepCount) {
+    stepper.setSpeed(SPEED);
+    buttonPressed();
+    stepper.runSpeed();
+}
+
+// Конвертирует joint state
 void writeMotors() {
 
     // Записываем шаговые двигатели
     for (int i = 0; i < MOTOR_STEP_COUNT; i++) {
-      
-      
+      long stepCount;
+      if (i == 0) {
+        nodeHandle.logwarn("test ----- ");
+        stepCount = targetJointPositions[0] * RADIAN * STEP_IN_ANGLE;
+
+        // с ускорением
+        // перед вызовом раскомментировать ускорение в initStepper
+        // stepperMoveTo(stepper_z, stepCount);
+
+        // без ускорения
+        stepperRun(stepper_z, stepCount);
+
+        nodeHandle.logwarn("test ----- end ");
+      }
     }
 
     // Записываем сервоприводы
@@ -88,15 +124,15 @@ void writeMotors() {
       int targetAngle;
 
       if (i == 0) {
-        targetAngle = targetJointPositions[3] *(90/3.14);
+        targetAngle = targetJointPositions[3] * (90/3.14);
       } else {
-        targetAngle = targetJointPositions[4] *(72/3.14);
+        targetAngle = targetJointPositions[4] * RADIAN;
       }
-      
+
       robotServos[i].write(targetAngle);
       motorCurrentPositions[i] = targetAngle;
     }
-    
+
   nodeHandle.spinOnce();
 }
 
@@ -107,14 +143,14 @@ void motorControlSubscriberCallbackJointState(const sensor_msgs::JointState& msg
   float joint_2 = msg.position[1];
   float joint_3 = msg.position[2];
   float joint_4 = msg.position[3] + M_PI;
-  float joint_grip = msg.position[4] + 2.50;
+  float joint_grip = msg.position[4] + M_PI/2;
 
   float joints_correct[MOTOR_COUNT] = {joint_1, joint_2, joint_3, joint_4, joint_grip};
 
-  if (joint_1 != joint_1_rem || 
-      joint_2 != joint_2_rem || 
-      joint_3 != joint_3_rem || 
-      joint_4 != joint_4_rem || 
+  if (joint_1 != joint_1_rem ||
+      joint_2 != joint_2_rem ||
+      joint_3 != joint_3_rem ||
+      joint_4 != joint_4_rem ||
       joint_grip != joint_grip_rem) {
 
         joint_1_rem = joint_1;
@@ -128,13 +164,13 @@ void motorControlSubscriberCallbackJointState(const sensor_msgs::JointState& msg
         char joint_3_current[8];
         char joint_4_current[8];
         char joint_grip_current[8];
-      
+
         dtostrf(joint_1, 1, 5, joint_1_current);
-        dtostrf(joint_2, 1, 5, joint_2_current); 
-        dtostrf(joint_3, 1, 5, joint_3_current); 
-        dtostrf(joint_4, 1, 5, joint_4_current); 
+        dtostrf(joint_2, 1, 5, joint_2_current);
+        dtostrf(joint_3, 1, 5, joint_3_current);
+        dtostrf(joint_4, 1, 5, joint_4_current);
         dtostrf(joint_grip, 1, 5, joint_grip_current);
-  
+
         nodeHandle.logerror("==============");
         nodeHandle.logwarn(joint_1_current);
         nodeHandle.logwarn(joint_2_current);
@@ -148,8 +184,12 @@ void motorControlSubscriberCallbackJointState(const sensor_msgs::JointState& msg
     targetJointPositions[i] = joints_correct[i];
   }
 
-  writeMotors();
+  buttonPressed();
 
+  // записываем на моторы только при нажатой кнопке
+  if (ledOn) {
+     writeMotors();
+  }
 }
 
 ros::Subscriber<sensor_msgs::JointState> motorControlSubscriberJointState("joint_states", &motorControlSubscriberCallbackJointState);
@@ -167,7 +207,8 @@ void initStepper(AccelStepper stepper, int stepPin, int dirPin, int enablePin) {
 
   stepper.move(0);
   stepper.setMaxSpeed(SPEED);
-  stepper.setSpeed(SPEED);
+  // stepper.setSpeed(SPEED);
+  // stepper.setAcceleration(ACCELERATION);
 }
 
 
@@ -183,41 +224,32 @@ void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 
-  
+
   // Инициализировать сервоприводы
   for (unsigned int i = 0; i < SERVO_COUNT; i++) {
     robotServos[i].attach(servo_pins[i]);
   }
-  
+
   nodeHandle.getHardware()->setBaud(115200);
   nodeHandle.initNode();
   nodeHandle.subscribe(motorControlSubscriberJointState);
 }
 
 void loop() {
-  start();
   nodeHandle.spinOnce();
   delay(1);
 }
 
-void start() {
-  buttonPressed();
-  runStepper();
-}
-
-void runStepper() {
-  if (ledOn) {
-
-    while (ledOn) {
-      positions[0] = -100; // Пример
-      positions[1] = 100;
-      
-      steppers.moveTo(positions);
-      steppers.runSpeedToPosition();
-      buttonPressed();
-    }
-  }
-}
+//void runStepper() {
+//  if (ledOn) {
+//
+//    while (ledOn) {
+//      steppers.moveTo(positions);
+//      steppers.runSpeedToPosition();
+//      buttonPressed();
+//    }
+//  }
+//}
 
 // стабилизация нажатия на кнопку
 boolean debounce (boolean last) {
