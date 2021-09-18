a0b4a7cdd4a07174fe1225e0bce1ef2cc8c2ce57
allicen <ellena.hella@yandex.ru>
2021-09-18 07:20:35 +0500

    Доработаны скрипты запуска.

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,24 +2,74 @@
 
 commandsFile="$(pwd)/data/commands.txt"
 commandsDescriptionFile="$(pwd)/data/command_description.txt"
+pidFile="$(pwd)/scripts/run.pid"
+
+echo "========="
+echo "Получены адреса файлов:"
+echo "1. $1"
+echo "2. $2"
+echo "========="
+
+function getPath {
+  firstChar=$(echo "$1" | cut -c1-1)
+  if [ "$firstChar" = '/' ] && [ -f "$1" ]; then
+    echo "$1"
+  elif [ -f "$(pwd)/$1" ]; then
+    echo "$(pwd)/$1"
+  else
+    echo NULL
+  fi
+}
+
+function readArgument {
+   if [[ "$(echo "$1" | cut -d "=" -f 1)" = "$2" ]]; then
+     echo "$1" | cut -d "=" -f 2
+   else
+     echo NULL
+   fi
+}
+
+commandsFirstArg=$(readArgument "$1" 'commands')
+commandsSecondArg=$(readArgument "$2" 'commands')
+descriptionFirstArg=$(readArgument "$1" 'description')
+descriptionSecondArg=$(readArgument "$2" 'description')
+
+if [[ $commandsFirstArg != NULL && $(getPath "$commandsFirstArg") != NULL ]]; then
+  commandsFile="$(getPath "$commandsFirstArg")"
+elif [[ $commandsSecondArg != NULL && $(getPath "$commandsSecondArg") != NULL ]]; then
+  commandsFile=$(getPath "$commandsSecondArg")
+fi
+
+if [[ $descriptionFirstArg != NULL && $(getPath "$descriptionFirstArg") != NULL ]]; then
+  commandsDescriptionFile="$(getPath "$descriptionFirstArg")"
+elif [[ $descriptionSecondArg != NULL && $(getPath "$descriptionSecondArg") != NULL ]]; then
+  commandsDescriptionFile="$(getPath "$descriptionSecondArg")"
+fi
+
+echo "Загружено описание команд из файла  '$commandsDescriptionFile'"
+echo "Загружена последовательность выполнения команд из файла '$commandsFile'"
+echo "========="
 
 declare -A commandsMap
 
 while IFS="" read -r commandDesc || [ -n "$commandDesc" ]
 do
-	key=$(echo $commandDesc | cut -d ":" -f 1)
-	value=$(echo $commandDesc | cut -d ":" -f 2)
+	key=$(echo "$commandDesc" | cut -d ":" -f 1)
+	value=$(echo "$commandDesc" | cut -d ":" -f 2)
 
 	commandsMap[$key]=$value
 
-done < $commandsDescriptionFile
+done < "$commandsDescriptionFile"
 
 while IFS="" read -r command || [ -n "$command" ]
 do
-	key=$(echo $command | cut -d " " -f 1)
-	delay=$(echo $command | cut -d " " -f 2)
+	key=$(echo "$command" | cut -d " " -f 1)
+	delay=$(echo "$command" | cut -d " " -f 2)
 
 	source devel/setup.bash
 	rosrun armbot_move position _param:="$key ${commandsMap[$key]} $delay"
 
-done < $commandsFile
+done < "$commandsFile"
+
+# Удалиить файл c PID после завершения процесса
+rm "$pidFile"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,74 @@
 #!/bin/bash
 
+pidFile="$(pwd)/scripts/run.pid"
+
+function killProcess {
+
+    local procPid=$(cat "$pidFile")
+    local re='^[0-9]+$'
+
+    if ! [[ $procPid =~ $re ]] ; then
+       echo "Error: PID must be a number" >&2; exit 1
+    fi
+
+    # Убить процесс и всех потомков
+    kill -- -"$(ps -o pgid= $procPid | grep -o [0-9]*)"
+
+    # удалить файл, где записан PID
+    rm "$pidFile"
+
+    echo "Процесс остановлен."
+}
+
 case "$1" in
     start)
-      echo "Starting the process..."
+      echo "Запускаю процесс..."
+      echo $$ > "$pidFile"
 
       # Заходит в docker
       if [[ "$2" = "docker" ]]; then
       sudo docker exec --tty -i armbot bash -c "cd workspace && sudo chmod +x scripts/*sh && 
-                                                sed -i -e 's/\r$//' scripts/run.sh && 
-                                                ./scripts/get_data.sh"
+                                                sed -i -e 's/\r$//' scripts/get_data.sh &&
+                                                ./scripts/get_data.sh $3 $4"
       else
         sudo chmod +x ./scripts/*sh
-        ./scripts/get_data.sh
+        ./scripts/get_data.sh "$2" "$3"
       fi
       ;;
-	*)
+
+    stop)
+      echo "Останавливаю процесс..."
+      if [ -f "$pidFile" ]; then
+            killProcess
+        else
+            echo "Файл с записью PID не существует"
+        fi
+    ;;
+
+    help)
+      echo "
+  Запустите скрипт ./run.sh с аргументами.
+
+  1 аргумент — название команды (обязателено). Значения:
+    · help — вызов справки;
+    · start — запуск скрипта;
+    · stop — остановка скрипта;
+    · restart — перезапуск скрипта.
+
+  2 аргумент — запуск с докером (необязателено). Значения:
+    · docker — запуск скрипта с докером;
+
+  3 аргумент и 4 аргумент — путь до файла со списком команд (необязательно) и путь до файла с описанием команд. Значения:
+    · commands=текстовая строка (относительный или абсолютный путь);
+    · description=текстовая строка (относительный или абсолютный путь).
+
+  commands — путь до файла со списком команд;
+  description — путь до файла с описанием команд.
+
+  Относительный без слеша (например, commands=scripts/commands.txt), абсолютный со слешем (например, commands=/home/user/scripts/commands.txt).
+  "
+      ;;
+	  *)
       echo "Command $1 not supported"
       ;;
 esac
