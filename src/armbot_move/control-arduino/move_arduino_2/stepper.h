b25e29583098f5f11dfe76562eff936901d40b50
allicen <ellena.hella@yandex.ru>
2021-10-15 01:07:25 +0500

    Добавлен возврат в исходную точку. Подключены шаговые двигатели.

--- src/armbot_move/control-arduino/move_arduino_2/stepper.h
+++ src/armbot_move/control-arduino/move_arduino_2/stepper.h
@@ -1,26 +1,25 @@
 #pragma once
 
 class Stepper {
-	public:
-		Stepper(int _stepPin, int _dirPin, int _enablePin) {
-			stepPin = _stepPin;
-			dirPin = _dirPin;
-			enablePin = _enablePin;
-		}
+    public:
+        Stepper(int _stepPin, int _dirPin, int _enablePin) {
+        stepPin = _stepPin;
+        dirPin = _dirPin;
+        enablePin = _enablePin;
+    }
 
-  AccelStepper getStepper() {
-      AccelStepper stepper(1, stepPin, dirPin);
-      stepper.setEnablePin(enablePin);
-      stepper.setPinsInverted(false, false, true);
-      stepper.enableOutputs();
-      stepper.setMaxSpeed(12000);
+    AccelStepper getStepper() {
+        AccelStepper stepper(1, stepPin, dirPin);
+        stepper.setEnablePin(enablePin);
+        stepper.setPinsInverted(false, false, true);
+        stepper.enableOutputs();
+        stepper.setMaxSpeed(12000);
 
-      return stepper;
-  } 
+        return stepper;
+    } 
 
-	private:
-		int stepPin;
-		int dirPin;
-		int enablePin;
-    
+    private:
+        int stepPin;
+        int dirPin;
+        int enablePin;
 };
