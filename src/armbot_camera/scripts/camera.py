32ebf8473765b65bdb3de7060b748895c4f07bab
Elena <ellena.hella@yandex.ru>
2022-05-04 00:10:12 +0500

    Обработка изображения с камеры

--- src/armbot_camera/scripts/camera.py
+++ src/armbot_camera/scripts/camera.py
@@ -2,6 +2,7 @@
 
 import time
 from math import sin, cos
+import numpy as np
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -37,9 +38,23 @@ class Camera():
 
         try:
             cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-            self.Image2 = cv_image
         except CvBridgeError, e:
-            rospy.logerr("CvBridge Error: {0}".format(e))        
+            rospy.logerr("CvBridge Error: {0}".format(e))\
+
+        line_type = 8
+        image_width = 600
+        image_height = 500
+        disable_part_image = 1.5
+
+        ppt = np.array([[0, 0],[0, image_height/disable_part_image],[image_width, image_height/disable_part_image],[image_width, 0]], np.int32)
+        ppt = ppt.reshape((-1, 1, 2))
+        cv2.fillPoly(cv_image, [ppt], (255, 255, 255), line_type)
+
+        grey_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(grey_image, 127, 255, cv2.THRESH_BINARY)
+        cv_image = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+        self.Image2 = cv_image
 
 
     def spin(self):
@@ -49,8 +64,8 @@ class Camera():
             self.rate.sleep()
 
             if self.Image1 is not None:
-                cv2.imshow("Down view camera from Robot", self.Image1)
-                cv2.imshow("Down2 view camera from Robot", self.Image2)
+                cv2.imshow("Room camera", self.Image1)
+                cv2.imshow("Robot camera", self.Image2)
                 cv2.waitKey(3)
 
 
